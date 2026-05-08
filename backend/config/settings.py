e2e83e7ec0e8e15efd48d5e93a8e0daaf22372b5
Mohammed ashik <ashiqmohamed886@gmail.com>
2026-05-08 21:46:09 +0530

    Fixed SECRET_KEY environment configuration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('django-insecure-o78cdr3%4ctjxvbz661eigtwdhy!fv3j%xmt$*3t(t6qnk3poi')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
